ae04f8e5b86e62358194428504a079d449d850a2
palinkiewicz <pogromca.ap@gmail.com>
2025-03-02 16:43:21 +0100

    Make Auth0 session remeberable

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
       audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       scope: "read:current_user update:current_user_metadata",
     }}
+    cacheLocation="localstorage"
     >
       <App />
     </Auth0Provider>
