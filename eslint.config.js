5b7b4773fe24831badf60b56931663fd6f387f17
iLoveReact <maksym.slotwinski2006@gmail.com>
2025-03-21 22:36:19 +0100

    feat(CC-7): Setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,91 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: [
+      'dist',
+      'node_modules',
+      'vite.config.ts',
+    ]
+  },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.recommended,
+      eslintConfigPrettier
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier
+    },
+    settings: {
+      react: { version: 'detect' }
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': ['error', { usePrettierrc: true }],
+
+      'import/prefer-default-export': 'off',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          'js': 'never',
+          'jsx': 'never',
+          'ts': 'never',
+          'tsx': 'never',
+          '': 'never'
+        }
       ],
-    },
-  },
+
+      'newline-before-return': 'warn',
+      'padding-line-between-statements': [
+        'warn',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: 'block-like' },
+        { blankLine: 'always', prev: 'directive', next: '*' }
+      ],
+      'eol-last': ['error', 'always'],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1, maxBOF: 0 }],
+      'object-curly-spacing': ['error', 'always'],
+
+      'eqeqeq': ['error', 'smart'],
+      'no-implicit-coercion': 'warn',
+      'no-unsafe-optional-chaining': 'error',
+      'no-prototype-builtins': 'warn',
+
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      '@typescript-eslint/strict-boolean-expressions': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
+      'no-shadow': 'error',
+      'sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+
+      'react/react-in-jsx-scope': 'off',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-boolean-value': ['warn', 'never'],
+      'react/jsx-props-no-spreading': 'off',
+      'react/require-default-props': 'off'
+    }
+  }
 )
