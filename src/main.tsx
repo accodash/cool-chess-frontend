5e12880c69e2124c5a70511841a38f036bb0b8f3
palinkiewicz <pogromca.ap@gmail.com>
2025-05-10 12:05:36 +0200

    Migrate to @mui/material

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './styles/index.scss';
 import App from './App.tsx';
 import { Auth0Provider } from '@auth0/auth0-react';
+import theme from './styles/theme.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,7 +22,9 @@ createRoot(document.getElementById('root')!).render(
       }}
       cacheLocation="localstorage"
     >
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Auth0Provider>
   </StrictMode>
 );
