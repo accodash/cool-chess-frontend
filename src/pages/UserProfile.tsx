4d557785553bc86eb5885e1a010b32dc12e23827
palinkiewicz <pogromca.ap@gmail.com>
2025-05-16 12:52:59 +0200

    Make match buttons friends-only

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -5,11 +5,13 @@ import { useCurrentUser } from '../hooks/useCurrentUser';
 import { useUserById } from '../hooks/useUserById';
 import UserProfileHeader from '../components/social/profile/UserProfileHeader';
 import RatingsGrid from '../components/social/profile/RatingsGrid';
+import { useFriends } from '../hooks/useFriends';
 
 export default function UserProfile() {
     const { id } = useParams();
     const { data: currentUser } = useCurrentUser();
     const { data: profileUser, isLoading } = useUserById(id || '');
+    const { data: friends } = useFriends();
 
     if (!profileUser || isLoading) return null;
 
@@ -18,11 +20,16 @@ export default function UserProfile() {
     const ratings: Record<string, number> = {};
     profileUser.ratings?.forEach((r) => (ratings[r.mode] = r.rating));
 
+    const friendRelation = friends?.find(
+        (relation) => relation.firstUser.uuid === id || relation.secondUser.uuid === id
+    );
+    const isFriend = !!friendRelation?.befriendedAt;
+
     return (
         <Box px={4} py={6}>
             <PageHeader title="User Profile" />
             <UserProfileHeader user={profileUser} isCurrentUser={isCurrentUser} />
-            <RatingsGrid ratings={ratings} showMatchButton={!isCurrentUser} />
+            <RatingsGrid ratings={ratings} showMatchButton={!isCurrentUser && isFriend} />
         </Box>
     );
 }
